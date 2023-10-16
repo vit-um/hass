3d147c34e11006fbbfda9f02ea1f40ebad1913dd
VITALII UMANETS <umanetsvitaliy@gmail.com>
2023-10-16 11:30:25 +0300

    repeater

--- zigbee2mqtt/TS0207_repeater.js
+++ zigbee2mqtt/TS0207_repeater.js
@@ -1,20 +1,5 @@
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
-const ota = require('zigbee-herdsman-converters/lib/ota');
 const tuya = require('zigbee-herdsman-converters/lib/tuya');
-const utils = require('zigbee-herdsman-converters/lib/utils');
-const globalStore = require('zigbee-herdsman-converters/lib/store');
-const legacy = require('zigbee-herdsman-converters/lib/legacy');
-const e = exposes.presets;
-const ea = exposes.access;
-
-const tzDatapoints = {
-    ...tuya.tz.datapoints,
-    key: [...tuya.tz.datapoints.key, 'radar_sensitivity', 'entry_sensitivity', 'illumin_threshold', 'detection_range','shield_range','entry_distance_indentation','entry_filter_time','departure_delay','block_time', 'status_indication','breaker_mode','breaker_status']
-}
 
 const definition = {
     fingerprint: [{ modelID: 'TS0207', manufacturerName: '_TZ3000_nlsszmzl'}],
